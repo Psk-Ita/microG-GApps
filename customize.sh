993a00797c5c625dcd7e016ec8e4de6ce34a3ea4
Psk-It <69162471+Psk-Ita@users.noreply.github.com>
2022-02-04 11:32:46 +0100

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,12 +1,5 @@
-
-apitest() 
-{
-    return expr 30 - $API
-}
-
-
-if apitest; then
-	rm -r "$MODPATH/system/priv-app/Phonesky"
-else 
-	rm -r "$MODPATH/system/priv-app/Vending"
+if [ $API -gt 30 ]; then
+  rm -r "$MODPATH/system/priv-app/Vending"
+else
+  rm -r "$MODPATH/system/priv-app/Phonesky"
 fi
