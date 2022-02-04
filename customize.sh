9cf12ca50e2093b6d23090c3b2df5cb7cb42904b
Psk-It <69162471+Psk-Ita@users.noreply.github.com>
2022-02-04 12:02:38 +0100

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-gms = (pm list package | grep 'com.google.android.gms' | wc -l)
-if [ $gms -gt 1 ]; then
-  pm install "$MODPATH/system/priv-app/GmsCore/com.google.android.gms.apk"
-fi
-
 if [ $API -gt 30 ]; then
   rm -r "$MODPATH/system/priv-app/Vending"
 else
   rm -r "$MODPATH/system/priv-app/Phonesky"
 fi
+
+pm install "$MODPATH/system/priv-app/GmsCore/com.google.android.gms.apk"
